a563a7a2215dadacbb4fb5a948055a7621841b5b
Akinlabi Mason-Hogans <admasonhogans@aggies.ncat.edu>
2018-11-17 23:10:03 -0500

    commit thiss three string

---
+++ Desktop/GITEM/OneString.java
@@ -0,0 +1,11 @@
+public class OneString {
+
+ public String toString() {
+   return "one string";
+ }
+ 
+  public static void main(String[] args) {
+    OneString obj = new OneString();
+    System.out.println("OneString object: " + obj);
+  }
+}
